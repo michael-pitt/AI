2a6aaef94ca8b1f0a766d16f5719bebb61569598
mpitt <Michael.Pitt@cern.ch>
2019-08-27 02:05:34 +0200

    exploration notebook

---
+++ ParticleImages/README.md
@@ -0,0 +1,44 @@
+# ParticleImages
+
+The "Particle Images" project is used to implement different characterization tasks from calorimeter images.
+Particle detector's calorimeter plays a role of image camera, but with varying granularity. 
+
+## Outline:
+* **[CalorimeterSegmentation](#calorimetersegmentation)** - Image segmentation of particle images
+* **[SuperResolution](#superresolution)** - Super Resolution (SR) tasks in particle detectors. Implementation of Super resolution for the task of neutral energy regression in calorimeter based particle detector.
+
+# Download 
+
+To download only this folder (clone a subdirectory from git repository) exetute the following lines:
+```bash
+git init
+git remote add origin https://USERNAME:PASSWORD@github.com/mpitt82/AI.git
+git config core.sparsecheckout true
+echo "ParticleImages/*" > .git/info/sparse-checkout
+git pull --depth=1 origin master
+```
+
+# Data exploration notebook
+Before performing different tasks on the calorimeter images, the [data exploration notebook](DataExploration.ipynb)
+ is used to explore simulation output. In this notebook, one can find, kinematic distribution of incoming particles,
+as well as the detector's energy response. A typical image obtained from all six calorimeter layers is shown in Figure 1.
+
+![Figure 1: Calorimeter image](images/Event_200_cells.png)
+
+# CalorimeterSegmentation
+This folder contains an implementation of segmentation of calorimeter cells.
+
+In particle calorimetry, segmentation of cell hits concerning different particle types plays a crucial role in various tasks:
+ measurement of the energy of jets, particle identification, and localization. 
+A few simple approaches are tested here to separate neutral pion from charged pion hits in the detector. 
+The detector hits are simulated using Geant4 simulation package with [ATLAS-simplified](https://mpitt82.github.io/Geant4-models/ATLAS-simplified) geometry (sampling calorimeter that comprises EM and Had layers).
+
+Example of a few simulation outputs can be found in [ATLAS-simplified@cernbox](https://cernbox.cern.ch/index.php/s/oCg3en1GHAvYSTo).
+
+# SuperResolution
+This folder contains an implementation of Super-Resolution task on images obtained form particle detectors. In contrast to computer vision tasks, particle detectors are 3D cameras incorporates different sensitive layers designed to stop particles that usually undergo two types of interactions, electromagnetic and hadronic. Often, all layers have different granularity and include various types of the response function, which make the task of super-resolution to be different from the mainstream computer vision tasks.
+
+
+The detector images obtained a simplified version of Geant4 based ATLAS detector simulation ([mpitt82/Geant4-models/ATLAS-simplified](https://github.com/mpitt82/Geant4-models/tree/master/ATLAS-simplified)). 
+
+Example of a few simulation outputs can be found in [link].
---
+++ ParticleImages/particleImages_helpers.py
@@ -0,0 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def printProgressBar (iteration, total, loss = '', decimals = 1, length = 50):
+    total = total - 1 #since usually we start from 0 till n-1
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = '#' * filledLength + '-' * (length - filledLength)
+    if(loss): print('\rprogress |%s| %s%% loss - %s' % (bar, percent, str(loss)), end = '\r')
+    else: print('\rprogress |%s| %s%%' % (bar, percent), end = '\r')
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
+def CellToMatrix(tree, nLayers = 6, nPixEta = 64, nPixPhi = 64, calorSizeXY = 144 * 16):
+    
+    cs_e,cs_x,cs_y,cs_dx,cs_dy,cs_l,cs_chfrac = tree.arrays(["cell_e",
+                                                         "cell_x",
+                                                         "cell_y",
+                                                         "cell_dx",
+                                                         "cell_dy",
+                                                         "cell_l",
+                                                         "cell_chfrac"]).values()
+    nevents = len(cs_e)
+    print('using total '+str(nevents)+' events')
+
+    cell_dEta = calorSizeXY // nPixEta;
+    cell_dPhi = calorSizeXY // nPixPhi;
+    
+    Ch_Energy = np.zeros((nevents,nLayers,nPixPhi,nPixEta))
+    Nu_Energy = np.zeros((nevents,nLayers,nPixPhi,nPixEta))
+
+    for i, (c_e, c_x, c_y, c_dx, c_dy, c_l, c_chfrac) in enumerate(zip(cs_e,cs_x,cs_y,cs_dx,cs_dy,cs_l,cs_chfrac)):
+        printProgressBar(i, nevents)
+        for e,x,y,dx,dy,l,f in zip(c_e, c_x, c_y, c_dx, c_dy, c_l, c_chfrac):
+            BinEtaMin = int(x-dx/2 + calorSizeXY/2) // cell_dEta
+            BinEtaMax = int(x+dx/2 + calorSizeXY/2) // cell_dEta
+            BinPhiMin = int(y-dy/2 + calorSizeXY/2) // cell_dPhi
+            BinPhiMax = int(y+dy/2 + calorSizeXY/2) // cell_dPhi
+            if(BinEtaMax==BinEtaMin): BinEtaMax=BinEtaMax+1
+            if(BinPhiMax==BinPhiMin): BinPhiMax=BinPhiMax+1
+            denom = (BinEtaMax-BinEtaMin)*(BinPhiMax-BinPhiMin) # democratic
+            for bin_eta in range(BinEtaMin,BinEtaMax):
+                for bin_phi in range(BinPhiMin,BinPhiMax):
+                    Ch_Energy[i][l-1][bin_phi][bin_eta] += e*f/denom
+                    Nu_Energy[i][l-1][bin_phi][bin_eta] += e*(1.0-f)/denom
+                    
+    return Ch_Energy, Nu_Energy
+	
+def DrawEvent(event, cellNu_Energy, cellCh_Energy):
+
+    fig, ax = plt.subplots(6, 3, figsize=(30, 60))
+    Title = ['Input Image', 'Charged Image', 'Neutral Image']
+    LayerNames=['ECAL1','ECAL2','ECAL3','HCAL1','HCAL2','HCAL3']
+
+    for layer_i in range(6):
+
+        input_image = cellNu_Energy[event][layer_i] + cellCh_Energy[event][layer_i]
+        target_nu_image = cellNu_Energy[event][layer_i]
+        target_ch_image = cellCh_Energy[event][layer_i]
+
+        ax[layer_i][0].imshow( input_image, cmap='plasma', vmin=0.001, vmax=10.8 )
+        ax[layer_i][1].imshow( target_ch_image, cmap='OrRd', vmin=0.001, vmax=10.8 )
+        ax[layer_i][2].imshow( target_nu_image, cmap='Greens', vmin=0.001, vmax=10.8 )
+
+        major_ticks = np.arange(-0.5, 63.5, 1)
+        minor_ticks = np.arange(-0.5, 63.5, 1)
+
+        ax[layer_i][0].set_ylabel(LayerNames[layer_i],fontsize=64)
+        for ipad in range(3) : 
+            ax[layer_i][ipad].set_xticks(minor_ticks, minor=True)
+            ax[layer_i][ipad].set_yticks(minor_ticks, minor=True)
+            ax[layer_i][ipad].grid(which='minor')
+            ax[0][ipad].set_title(Title[ipad]+' ' , fontsize=48)     
+        ax[layer_i][0].text(5,5,'E = %2.2f GeV'%(input_image.sum()/1e3),fontsize=64,bbox={'facecolor': 'white'})
+        ax[layer_i][1].text(5,5,'E = %2.2f GeV'%(target_ch_image.sum()/1e3),fontsize=64,bbox={'facecolor': 'white'})
+        ax[layer_i][2].text(5,5,'E = %2.2f GeV'%(target_nu_image.sum()/1e3),fontsize=64,bbox={'facecolor': 'white'})
+
+    plt.tight_layout()
+    fig.savefig('images/Event_' + str(event) + '_cells.pdf')
+    fig.savefig('images/Event_' + str(event) + '_cells.png')
+    plt.show()
+	
